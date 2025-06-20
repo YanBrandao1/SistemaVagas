f288bb1d8fa9369abbb57594871ea62d33b40755
YanBrandao1 <yanbbs00@gmail.com>
2025-06-20 09:35:47 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaVagas/Models/FirebaseAuthResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVagas.Models
+{
+    class FirebaseAuthResponse
+    {
+        public string IdToken { get; set; }          // JWT Token
+        public string Email { get; set; }
+        public string RefreshToken { get; set; }
+        public string ExpiresIn { get; set; }
+        public string LocalId { get; set; }          // User ID
+        public string Registered { get; set; }       // Para login
+    }
+}
---
+++ SistemaVagas/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SistemaVagas;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
